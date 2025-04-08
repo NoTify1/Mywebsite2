2307ee0a3db1506ffbb086f0d84e8106eb416fdc
NoTify1 <sujalgiri1122@gmail.com>
2025-04-08 09:52:22 +0545

    Update and rename script.js to signup.html

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Example: Toggle dark mode
-    const toggleButton = document.querySelector('#toggle-dark-mode');
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('bg-white');
-        document.body.classList.toggle('text-black');
-    });
-});
