2ff84de0c65c626637fe54a5046397ca7c238a8e
NoTify1 <sujalgiri1122@gmail.com>
2025-04-08 09:44:26 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    // Example: Toggle dark mode
+    const toggleButton = document.querySelector('#toggle-dark-mode');
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('bg-white');
+        document.body.classList.toggle('text-black');
+    });
+});
