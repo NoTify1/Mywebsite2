1e702da8f2b3e8acdb744089780ae0bd9740496e
NoTify1 <sujalgiri1122@gmail.com>
2025-04-08 11:07:23 +0545

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,14 +1,1 @@
-document.getElementById('signin-form').addEventListener('submit', function (e) {
-    e.preventDefault();
 
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-
-    if (!email || !password) {
-        alert("Please fill in both fields.");
-        return;
-    }
-
-    alert("Sign In successful!");
-    window.location.href = "dashboard.html"; // Redirect to dashboard after successful sign-in
-});
