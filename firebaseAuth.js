20649dae035cbe650d4de878b9b8a0aff8d6be91
NoTify1 <sujalgiri1122@gmail.com>
2025-04-12 18:18:58 +0545

    Update and rename register.js to firebaseAuth.js

---
+++ firebaseAuth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCMPqyAPQZr-FQJDj9Vo9S4N3-X2bwFltw",
+    authDomain: "notify-backend-a59ce.firebaseapp.com",
+    projectId: "notify-backend-a59ce",
+    storageBucket: "notify-backend-a59ce.firebasestorage.app",
+    messagingSenderId: "840399313242",
+    appId: "1:840399313242:web:425d2e6fc4bc19f63cfb63"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
