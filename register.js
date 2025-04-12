f118ec8cf4e9628079ce81c48352f6402b2243f0
NoTify1 <sujalgiri1122@gmail.com>
2025-04-12 11:59:30 +0545

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZqBi-LT6qtbG2DuOdQwupBURjeWpyMak",
+    authDomain: "notify-backend-97e2a.firebaseapp.com",
+    projectId: "notify-backend-97e2a",
+    storageBucket: "notify-backend-97e2a.firebasestorage.app",
+    messagingSenderId: "125388997985",
+    appId: "1:125388997985:web:a33850f2e885511ef4f120"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
